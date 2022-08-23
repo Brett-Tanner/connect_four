c919a1b4d47c74011671b597488d27d60fc9b68c
Brett Tanner <bretttanner171@gmail.com>
2022-08-23 21:22:54 +0900

    Test #get_coordinates

--- lib/game.rb
+++ lib/game.rb
@@ -1,20 +1,49 @@
 class Game
 
-  EMPTY_SLOT = " ○ "
-  WHITE_PIECE = " ⚪ "
-  BLACK_PIECE = " ⚫ "
+  EMPTY = " ○ "
+  WHITE = " ⚪ "
+  BLACK = " ⚫ "
 
   def initialize
-    @board = Array.new(4) {Array.new(4, EMPTY_SLOT)}
+    @board = Array.new(4) {Array.new(4, EMPTY)}
     @turn_count = 0
   end
   
   def play_game
     print_board()
-    # players take turns
-    # check if game is over
+    puts "#{BLACK} goes first"
+    # loop 
+      # play_turn(BLACK)
+      # break if game_over?
+      # play_turn(WHITE)
+      # break if game_over?
     # declare winner
     # ask if they wanna play again
+  end
+
+  def play_turn(player)
+    coordinates = get_coordinates(player)
+    row = coordinates[0] - 1
+    col = coordinates[1] - 1
+    # update board if unoccupied and in bounds
+  end
+
+  def get_coordinates(player)
+    puts "#{player}, enter coordinates like 'row, col'"
+    input = gets.chomp.split(", ")
+    if input.length > 2
+      puts "**You entered too many numbers**"
+      return get_coordinates(player)
+    end
+    input.map {|char| char.to_i}
+  end
+
+  def valid_move?(row, col)
+    
+  end
+
+  def game_over?
+    
   end
 
   private
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -4,6 +4,10 @@ describe "Game" do
   subject(:game) {Game.new}
   let(:board) {game.instance_variable_get(:@board)}
 
+  before do
+    allow(game).to receive(:puts)
+  end
+
   context "when initialized" do
     it "creates a 4x4 board" do
       num_of_rows = board.length
@@ -18,8 +22,64 @@ describe "Game" do
     end
   end
 
-  describe "#take_turn" do
+  describe "#get_coordinates" do
+    let(:player) {("⚫")}
+    let(:coordinates) {game.get_coordinates(player)}
+
+    context "when given valid coordinates" do
+
+      before do
+        allow(game).to receive(:gets).and_return("1, 4")
+      end
+
+      it "returns an array" do
+        expect(coordinates).to be_an_instance_of Array
+      end
+  
+      it "of two values" do
+        length = coordinates.length
+        expect(length).to eql(2)
+      end
+  
+      it "both integers" do
+        row = coordinates[0]
+        col = coordinates[1]
+        expect(row).to be_an_instance_of Integer
+        expect(col).to be_an_instance_of Integer
+      end
+    end
+
+    context "when input is too long" do
+
+      before do
+        allow(game).to receive(:gets).and_return("3, 5, 7, 10", "2, 3")
+      end
+
+      it "displays error" do
+        error = "**You entered too many numbers**"
+        expect(game).to receive(:puts).with(error).once
+        game.get_coordinates(player)
+      end
+
+      it "calls self" do
+        expect(game).to receive(:get_coordinates).once
+        game.get_coordinates(player)
+      end
+
+      it "returns the valid coordinates" do
+        valid_coord = [2, 3]
+        return_val = game.get_coordinates(player)
+        expect(return_val).to eql(valid_coord)
+      end
+    end
+  end
+  
+  describe "#valid_move?" do
     
+  end
+
+  describe "#play_turn" do
+    # you can actually measure changes btw, check the bookmark
   end
 
   describe "#play_game" do
